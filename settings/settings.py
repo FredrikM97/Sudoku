4cdd42bb30a7d62ba8696a436c80d1aa8baabe31
FredrikM97 <fredrik9779@gmail.com>
2020-02-05 17:39:40 +0100

    Setting file for future work

---
+++ settings/settings.py
@@ -0,0 +1,15 @@
+from Theme.default import defaultTheme
+
+class SETTINGS:
+    default_settings = {
+            'theme': defaultTheme,
+            'darktheme': False,
+            'difficulty':1,
+            'board_base':3,
+        }
+    user_settings = {
+            'theme': defaultTheme,
+            'darktheme': False,
+            'difficulty':1,
+            'board_base':3,
+        }
