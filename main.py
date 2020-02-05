c2f7f01bef5bef619f1465e45e4f64cd60634a3d
FredrikM97 <fredrik9779@gmail.com>
2020-02-05 03:28:35 +0100

    Improved main and init for GUI

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-
+from PyQt5.QtWidgets import QApplication
 from board import Board
+from graphic import mainGUI
+import sys
 
 def main():
+    init_GUI()
+
+def init_GUI():
+    app = QApplication(sys.argv)
+    game = mainGUI()
+    game.show()
+    sys.exit(app.exec_())
+
+def init_board():
     board = Board(base=3)
-    board.run()
     print("Board:")
-    board.pretty_prints(board.board)
+    board.pretty_prints(board.tiles)
     print("Solved board:")
-    board.pretty_prints(board.SolvedBoard)
+    board.pretty_prints(board.solvedTiles)
 
 
 if __name__ == "__main__":
