607672d9a1c5332ba58859fbd1d1c757af7b7756
FredrikM97 <fredrik9779@gmail.com>
2020-02-05 21:31:28 +0100

    Created config file for settings

---
+++ settings/config.py
@@ -0,0 +1,15 @@
+from settings.theme import *
+
+
+default_settings = {
+        'theme': default,
+        'darktheme': False,
+        'difficulty':5,
+        'board_base':3,
+    }
+user_settings = {
+        'theme': default,
+        'darktheme': False,
+        'difficulty':5,
+        'board_base':3,
+    }
