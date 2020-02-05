a1e3ed581ad14717595444cb1da52949410b0819
FredrikM97 <fredrik9779@gmail.com>
2020-02-05 21:30:02 +0100

    Moved and renamed theme file

---
+++ settings/theme.py
@@ -0,0 +1,67 @@
+
+class default:
+    # Empty Tiles
+    empty_tile = (
+        'background-color: rgb(255, 255, 255);'
+		'color: rgb(255, 255, 255);'
+    )
+    # Focused empty Tiles
+    focused_empty_tile = (
+        'background-color: rgb(55, 58, 64);'
+		'color: rgb(55, 58, 64);'
+    )
+    # Generated Tiles
+    static_tile = (
+        'background-color: rgb(222, 222, 222);'
+		'color: rgb(0, 0, 0);'
+		'font-size: 25pt; font-family: "Ubuntu";'
+    )
+    # Widget background 
+    widget_background = (
+        'background-color: rgb(247, 247, 247);'
+    )
+    # Edited tile
+    dynamic = (
+        'background-color: rgb(255, 255, 255);'
+        'color: rgb(0, 0, 0);'
+        'font-size: 25pt; font-family: "Ubuntu";'
+    )
+    # Focused edited tile
+    focus_dynamic = (
+        'background-color: rgb(255, 245, 225);'
+        'color: rgb(0, 0, 0);'
+        'font-size: 25pt; font-family: "Ubuntu";'		
+    )
+class Example1:
+    # Empty Tiles
+    empty_tile = (
+        'background-color: rgb(255, 255, 255);'
+		'color: rgb(255, 255, 255);'
+    )
+    # Focused empty Tiles
+    focused_empty_tile = (
+        'background-color: rgb(55, 58, 64);'
+		'color: rgb(55, 58, 64);'
+    )
+    # Generated Tiles
+    static_tile = (
+        'background-color: rgb(222, 222, 222);'
+		'color: rgb(0, 0, 0);'
+		'font-size: 25pt; font-family: "Ubuntu";'
+    )
+    # Widget background 
+    widget_background = (
+        'background-color: rgb(0, 247, 247);'
+    )
+    # Edited tile
+    dynamic = (
+        'background-color: rgb(255, 255, 255);'
+        'color: rgb(0, 0, 0);'
+        'font-size: 25pt; font-family: "Ubuntu";'
+    )
+    # Focused edited tile
+    focus_dynamic = (
+        'background-color: rgb(255, 245, 225);'
+        'color: rgb(0, 0, 0);'
+        'font-size: 25pt; font-family: "Ubuntu";'		
+    )
