81100f3e5a7113a937cc8e99d042bbf205973217
FredrikM97 <fredrik9779@gmail.com>
2020-02-05 03:27:15 +0100

    Added default theme

---
+++ Theme/default.py
@@ -0,0 +1,33 @@
+class defaultTheme:
+    # Empty Tiles
+    empty_tile = (
+        'background-color: rgb(255, 255, 255);'
+		'color: rgb(255, 255, 255);'
+    )
+    # Focused empty Tiles
+    focused_empty_tile = (
+        'background-color: rgb(55, 58, 64);'
+		'color: rgb(55, 58, 64);'
+    )
+    # Generated Tiles
+    static_tile = (
+        'background-color: rgb(222, 222, 222);'
+		'color: rgb(0, 0, 0);'
+		'font-size: 25pt; font-family: "Ubuntu";'
+    )
+    # Widget background 
+    widget_background = (
+        'background-color: rgb(58, 60, 64);'
+    )
+    # Dynamic cell (user specified)
+    dynamic_ss = (
+        'background-color: rgb(255, 255, 255);'
+        'color: rgb(0, 0, 0);'
+        'font-size: 25pt; font-family: "Ubuntu";'
+    )
+    # In-focus dynamic cells
+    focus_dynamic_ss = (
+        'background-color: rgb(255, 245, 225);'
+        'color: rgb(0, 0, 0);'
+        'font-size: 25pt; font-family: "Ubuntu";'		
+    )
